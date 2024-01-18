c379e00cfcdfe63caaf1740e9d666a64850223be
khoa-truong <khoa-truong@cybozu.vn>
2024-01-18 22:38:02 +0700

    feat: add icon registry

--- App.tsx
+++ App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
 import {NavigationContainer} from '@react-navigation/native';
-import {ApplicationProvider} from '@ui-kitten/components';
+import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
 import {publicRoutes} from '~/constants/routes';
 import {PublicNavigator, PublicScreen} from '~/libs/navigation';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
 
 function App() {
   return (
-    <ApplicationProvider {...eva} theme={eva.light}>
-      <NavigationContainer>
-        <PublicNavigator>
-          {publicRoutes.map(route => (
-            <PublicScreen
-              key={route.name}
-              name={route.name}
-              component={route.screen}
-              options={route.options}
-            />
-          ))}
-        </PublicNavigator>
-      </NavigationContainer>
-    </ApplicationProvider>
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva.dark}>
+        <NavigationContainer>
+          <PublicNavigator>
+            {publicRoutes.map(route => (
+              <PublicScreen
+                key={route.name}
+                name={route.name}
+                component={route.screen}
+                options={route.options}
+              />
+            ))}
+          </PublicNavigator>
+        </NavigationContainer>
+      </ApplicationProvider>
+    </>
   );
 }
 
